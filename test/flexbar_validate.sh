2a429ac4ad13cbe478008c0656f27bbc0fd3c465
jtroehr <johannes.roehr@fu-berlin.de>
2015-09-24 19:30:31 +0200

    Remove color-space tests

--- test/flexbar_validate.sh
+++ test/flexbar_validate.sh
@@ -5,14 +5,8 @@ echo ""
 echo "Testing fasta:"
 ./flexbar_test_fasta.sh
 
-echo "Testing csfasta:"
-./flexbar_test_csfasta.sh
-
 echo "Testing fastq:"
 ./flexbar_test_fastq.sh
-
-echo "Testing csfastq:"
-./flexbar_test_csfastq.sh
 
 echo "Testing decompression:"
 ./flexbar_test_zip.sh
