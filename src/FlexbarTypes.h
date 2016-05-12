0f7272dec6ab2923dcbf993c8e6335d90e0162e3
jtroehr <johannes.roehr@fu-berlin.de>
2016-05-12 23:45:02 +0200

    Extend types

--- src/FlexbarTypes.h
+++ src/FlexbarTypes.h
@@ -30,11 +30,37 @@ class SeqRead {
 		  qual(quality){
 	}
 	
-	virtual ~SeqRead(){};
+	virtual ~SeqRead(){}
 };
 
 
-#include "PairedRead.h"
+template <typename TSeqStr, typename TString>
+class PairedRead {
+
+	public:
+	
+	typedef SeqRead<TSeqStr, TString> TSeqRead;
+	
+	TSeqRead *m_r1, *m_r2, *m_b;
+	
+	TSeqStr m_randTag;
+	int m_barcode_id, m_barcode_id2;
+	
+	PairedRead(TSeqRead *r1, TSeqRead *r2, TSeqRead *b) :
+		m_r1(r1),
+		m_r2(r2),
+		m_b(b),
+		m_barcode_id(0),
+		m_barcode_id2(0),
+		m_randTag(""){
+	}
+	
+	virtual ~PairedRead(){
+		delete m_r1;
+		delete m_r2;
+		delete m_b;
+	}
+};
 
 
 namespace flexbar{
