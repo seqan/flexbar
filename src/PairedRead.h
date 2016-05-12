553d57b542c94f5eef3185dd6dbf81ca5d9bfc03
jtroehr <johannes.roehr@fu-berlin.de>
2016-05-12 23:44:01 +0200

    Minor changes

--- src/PairedRead.h
+++ src/PairedRead.h
@@ -8,33 +8,5 @@
 #define FLEXBAR_PAIREDREAD_H
 
 
-template <typename TSeqStr, typename TString>
-class PairedRead {
-
-public:
-	
-	typedef SeqRead<TSeqStr, TString> TSeqRead;
-	
-	TSeqRead *m_r1, *m_r2, *m_b;
-	
-	TSeqStr m_randTag;
-	int m_barcode_id, m_barcode_id2;
-	
-	PairedRead(TSeqRead *r1, TSeqRead *r2, TSeqRead *b) :
-		m_r1(r1),
-		m_r2(r2),
-		m_b(b),
-		m_barcode_id(0),
-		m_barcode_id2(0),
-		m_randTag(""){
-	};
-	
-	virtual ~PairedRead(){
-		delete m_r1;
-		delete m_r2;
-		delete m_b;
-	};
-	
-};
 
 #endif
