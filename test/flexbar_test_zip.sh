dbeec9012ab7c8443f76a525ce10c3b5f437f466
jtroehr <johannes.roehr@fu-berlin.de>
2016-05-24 17:33:56 +0200

    Adjusted test files

--- test/flexbar_test_zip.sh
+++ test/flexbar_test_zip.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
-flexbar --reads reads.fastq.gz --target result_gz --adapter-min-overlap 4 --adapters adapters.fasta --min-read-length 10 --adapter-threshold 1 --adapter-trim-end RIGHT > /dev/null
+flexbar --reads reads.fastq.gz --target result_gz --adapter-min-overlap 4 --adapters adapters.fasta --min-read-length 10 --adapter-error-rate 1 --adapter-trim-end RIGHT > /dev/null
 
 a=`diff correct_result_right.fastq result_gz.fastq`
 
@@ -13,7 +13,7 @@ echo "Test gzip OK"
 fi
 
 
-flexbar --reads reads.fastq.bz2 --target result_bz2 --adapter-min-overlap 4 --adapters adapters.fasta --min-read-length 10 --adapter-threshold 1 --adapter-trim-end RIGHT > /dev/null
+flexbar --reads reads.fastq.bz2 --target result_bz2 --adapter-min-overlap 4 --adapters adapters.fasta --min-read-length 10 --adapter-error-rate 1 --adapter-trim-end RIGHT > /dev/null
 
 a=`diff correct_result_right.fastq result_bz2.fastq`
 
