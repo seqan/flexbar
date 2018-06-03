76a21906203bf39912ba2753f01761cb7defcf2f
jtroehr <johannes.roehr@fu-berlin.de>
2018-06-03 20:00:57 +0200

    Update version

--- src/Flexbar.cpp
+++ src/Flexbar.cpp
@@ -2,7 +2,7 @@
    
    Flexbar - flexible barcode and adapter removal
    
-   Version 3.3.0
+   Version 3.4.0
    
    BSD 3-Clause License
    
@@ -29,7 +29,7 @@ int main(int argc, const char* argv[]){
 	using namespace std;
 	using namespace seqan;
 	
-	const string version = "3.3.0";
+	const string version = "3.4 beta";
 	const string date    = "June 2018";
 	
 	ArgumentParser parser("flexbar");
