1a421f89fe09ad129914f60a01c1bab9c81aed4d
jtroehr <johannes.roehr@fu-berlin.de>
2018-06-06 16:02:31 +0200

    Illumina adapter sequences

---
+++ src/Adapters.h
@@ -0,0 +1,42 @@
+// Adapters.h
+
+// Oligonucleotide sequences © 2018 Illumina, Inc.  All rights reserved.
+// Obtained from https://support.illumina.com/bulletins/2016/12/what-sequences-do-i-use-for-adapter-trimming.html
+
+#ifndef FLEXBAR_ADAPTERS_H
+#define FLEXBAR_ADAPTERS_H
+
+namespace flexbar{
+	
+	Adapters TrueSeq_ltht = Adapters("TruSeq-LT-HT");
+	TrueSeq_ltht.seq1 = "AGATCGGAAGAGCACACGTCTGAACTCCAGTCA";
+	TrueSeq_ltht.seq2 = "AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT";
+	TrueSeq_ltht.info = "TruSeq LT and TruSeq HT-based kits";
+	
+	Adapters TrueSeq_methyl = Adapters("TrueSeq-methyl");
+	TrueSeq_methyl.seq1 = "AGATCGGAAGAGCACACGTCTGAAC";
+	TrueSeq_methyl.seq2 = "AGATCGGAAGAGCGTCGTGTAGGGA";
+	TrueSeq_methyl.info = "ScriptSeq and TruSeq DNA Methylation";
+	
+	Adapters TrueSeq_smallRNA = Adapters("TrueSeq-smallRNA");
+	TrueSeq_smallRNA.seq1 = "TGGAATTCTCGGGTGCCAAGG";
+	TrueSeq_smallRNA.info = "TruSeq Small RNA";
+	
+	Adapters TrueSeq_ribo = Adapters("TrueSeq-ribo");
+	TrueSeq_ribo.seq1 = "AGATCGGAAGAGCACACGTCT";
+	TrueSeq_ribo.info = "TruSeq Ribo Profile";
+	
+	Adapters Nextera_TruSight = Adapters("Nextera-TruSight");
+	Nextera_TruSight.seq1 = "CTGTCTCTTATACACATCT";
+	Nextera_TruSight.seq2 = "CTGTCTCTTATACACATCT";
+	Nextera_TruSight.info = "AmpliSeq, Nextera, Nextera DNA Flex, Nextera DNA, Nextera XT, Nextera Enrichment, Nextera Rapid Capture Enrichment, TruSight Enrichment, TruSight Rapid Capture Enrichment, TruSight HLA";
+	
+	Adapters Nextera_matepair = Adapters("Nextera-matepair");
+	Nextera_matepair.seq1 = "GATCGGAAGAGCACACGTCTGAACTCCAGTCAC";
+	Nextera_matepair.seq2 = "GATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT";
+	Nextera_matepair.seqc = "CTGTCTCTTATACACATCT";
+	Nextera_matepair.info = "Nextera Mate Pair";
+	
+}
+
+#endif
