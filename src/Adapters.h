cb26a5a0ef49b8c6f16e10ff4dfc95c735b09e80
jtroehr <johannes.roehr@fu-berlin.de>
2018-06-07 16:06:32 +0200

    Update adapters

--- src/Adapters.h
+++ src/Adapters.h
@@ -8,12 +8,12 @@
 
 namespace flexbar{
 	
-	Adapters TrueSeq_ltht = Adapters("TruSeq-LT-HT");
+	Adapters TrueSeq_ltht = Adapters("TruSeq");
 	TrueSeq_ltht.seq1 = "AGATCGGAAGAGCACACGTCTGAACTCCAGTCA";
 	TrueSeq_ltht.seq2 = "AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT";
 	TrueSeq_ltht.info = "TruSeq LT and TruSeq HT-based kits";
 	
-	Adapters TrueSeq_methyl = Adapters("TrueSeq-methyl");
+	Adapters TrueSeq_methyl = Adapters("TrueSeq-Methyl");
 	TrueSeq_methyl.seq1 = "AGATCGGAAGAGCACACGTCTGAAC";
 	TrueSeq_methyl.seq2 = "AGATCGGAAGAGCGTCGTGTAGGGA";
 	TrueSeq_methyl.info = "ScriptSeq and TruSeq DNA Methylation";
@@ -22,7 +22,7 @@ namespace flexbar{
 	TrueSeq_smallRNA.seq1 = "TGGAATTCTCGGGTGCCAAGG";
 	TrueSeq_smallRNA.info = "TruSeq Small RNA";
 	
-	Adapters TrueSeq_ribo = Adapters("TrueSeq-ribo");
+	Adapters TrueSeq_ribo = Adapters("TrueSeq-Ribo");
 	TrueSeq_ribo.seq1 = "AGATCGGAAGAGCACACGTCT";
 	TrueSeq_ribo.info = "TruSeq Ribo Profile";
 	
@@ -31,7 +31,7 @@ namespace flexbar{
 	Nextera_TruSight.seq2 = "CTGTCTCTTATACACATCT";
 	Nextera_TruSight.info = "AmpliSeq, Nextera, Nextera DNA Flex, Nextera DNA, Nextera XT, Nextera Enrichment, Nextera Rapid Capture Enrichment, TruSight Enrichment, TruSight Rapid Capture Enrichment, TruSight HLA";
 	
-	Adapters Nextera_matepair = Adapters("Nextera-matepair");
+	Adapters Nextera_matepair = Adapters("Nextera-Matepair");
 	Nextera_matepair.seq1 = "GATCGGAAGAGCACACGTCTGAACTCCAGTCAC";
 	Nextera_matepair.seq2 = "GATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT";
 	Nextera_matepair.seqc = "CTGTCTCTTATACACATCT";
